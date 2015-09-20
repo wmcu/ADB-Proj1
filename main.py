aeb14828035b33b1b0f893190caa9c5496c99f25
Meng Wang <wm19922009@gmail.com>
2015-09-20 15:14:20 -0400

    First run success

--- main.py
+++ main.py
@@ -10,28 +10,31 @@ def main(account_key, precision, queries):
     # print "precision:", precision
     # print "query:", queries
 
-    # Search web pages by given queries
-    results = search_web(account_key, queries)
-    if len(results) < 10:
-        print "Too few docs found, done"
-        break
+    while True:
+        # Search web pages by given queries
+        results = search_web(account_key, queries)
+        print len(results)
+        if len(results) < 10:
+            print "Too few docs found, done"
+            break
 
-    # Collect user feedback
-    feedbacks = collect_feedback(results)
-    this_precision = 1.0 * sum(feedbacks) / len(feedbacks)
-    if this_precision == 0:
-        print "No relevant doc found, abort"
-        break
-    if this_precision >= precision:
-        print "Desired precision reached, done"
-        break
-
-    # Expand queries by relevance feedback
-    new_queries = expand_query(queries, results, feedbacks)
+        # Collect user feedback
+        feedbacks = collect_feedback(results)
+        this_precision = 1.0 * sum(feedbacks) / len(feedbacks)
+        print "Precision:", this_precision
+        if this_precision == 0:
+            print "No relevant doc found, abort"
+            break
+        if this_precision >= precision:
+            print "Desired precision reached, done"
+            break
+        print "================================="
+        # Expand queries by relevance feedback
+        queries = expand_query(queries, results, feedbacks)
 
 
 if __name__ == '__main__':
     account_key = sys.argv[1]
-    precision = sys.argv[2]
+    precision = float(sys.argv[2])
     queries = sys.argv[3].split()
     main(account_key, precision, queries)
