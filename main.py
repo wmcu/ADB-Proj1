00ccaf7c6c52c4e9f8dce112ab90a99ba7f4a155
Meng Wang <wm19922009@gmail.com>
2015-09-20 14:07:40 -0400

    Read command line arguments

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import base64
 import urllib2
+import sys
+
+def main(account_key, precision, query):
+    print "account_key:", account_key
+    print "precision:", precision
+    print "query:", query
 
 if __name__ == '__main__':
-	main()
+    account_key = sys.argv[1]
+    precision = sys.argv[2]
+    query = sys.argv[3]
+    main(account_key, precision, query)
